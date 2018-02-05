20464deeb72784a834a31f99aa05d14c2d032022
Nikhil_Kumaran <nikhil.kumaran.2011@gmail.com>
2018-02-05 12:44:18 +0530

    added post_details

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name="post_list")
+    url(r'^$', views.home, name="home"),
+    url(r'^posts/$', views.post_list, name="post_list"),
+    url(r'^posts/details/(?P<id>[\d]+)/$',views.post_details, name="details")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>HOME PAGE</h1><br><a href = 'posts'><h2>POSTS</h2></a>")
+
 def post_list(request):
     posts = Post.objects.all().order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_details(request, id):
+    post = get_object_or_404(Post, id = id)
+    return render(request, 'blog/post_details.html', {'post':post})
